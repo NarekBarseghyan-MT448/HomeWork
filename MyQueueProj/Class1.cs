da6127cb8c9cf273a0be3162011f479cb88b36af
NarekBarseghyan-MT448 <narekbarseghyan.mt448@polytechnic.am>
2026-03-13 08:04:05 +0400

    HW1

---
+++ MyQueueProj/Class1.cs
@@ -0,0 +1,55 @@
+﻿using LinkedListProj;
+using System.Collections;
+
+namespace MyQueueProj;
+
+public class MyQueue<T> : IEnumerable<T>
+{
+    MyLinkedList<T> items = new MyLinkedList<T>();
+
+    public void Enqueue(T item)
+    {
+        items.AddLast(item);
+    }
+
+    public T Dequeue()
+    {
+        if (items.Count == 0)
+            throw new InvalidOperationException("Queue is empty");
+        T value = items.Head.Value;
+        items.RemoveFirst();
+        return value;
+    }
+
+    public T peek()
+    {
+        if (items.Count == 0)
+            throw new InvalidOperationException("Queue is empty ");
+        return items.Head.Value;
+    }
+
+    public int Count
+    {
+        get { return items.Count; }
+    }
+
+    public void Clear()
+    {
+        items.Clear();
+        Console.WriteLine("Հերթը դատարկվեց:");
+    }
+
+    public IEnumerator<T> GetEnumerator()
+    {
+        throw new NotImplementedException();
+    }
+
+    IEnumerator IEnumerable.GetEnumerator()
+    {
+        return GetEnumerator();
+    }
+
+
+
+
+}
---
+++ TestLinkedList/Program.cs
@@ -0,0 +1,46 @@
+﻿using ClassLibrary1;
+using MyStackProj;
+
+public class Program
+{
+    private static void Main()
+    {
+        Console.WriteLine("=======Hello Void=======");
+
+        var linkedList = new MyLinkedList<string>();
+        linkedList.AddLast("5");
+        linkedList.AddLast("6");
+        linkedList.AddLast("7");
+        linkedList.AddLast("*");
+        linkedList.AddLast("+");
+        linkedList.AddLast("1");
+        linkedList.AddLast("-");
+
+        var math = new List<string> { "+", "-", "/", "*" };
+        MyStack<int> stack = new MyStack<int>();
+
+        foreach (var item in linkedList)
+        {
+            string token = item.ToString();
+            if (math.Contains(token))
+            {
+                int val2 = stack.Pop();
+                int val1 = stack.Pop();
+
+                switch (token)
+                {
+                    case "+": stack.Push(val1 + val2); break;
+                    case "-": stack.Push(val1 - val2); break;
+                    case "*": stack.Push(val1 * val2); break;
+                    case "/": stack.Push(val1 / val2); break;
+                }
+            }
+            else if (int.TryParse(token, out int num))
+            {
+                stack.Push(num);
+            }
+        }
+
+        Console.WriteLine($"Result: {stack.Pop()}"); // Output: 46
+    }
+}
